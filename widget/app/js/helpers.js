ff5aeceb0ffceefb38aae6031e19a90a17c24c23
Pavel Kinev <kinev@loyalty-rewarded.com>
2016-03-20 22:14:23 +0200

    - adding API methods, most of them accessible through the console

--- widget/app/js/helpers.js
+++ widget/app/js/helpers.js
@@ -23,3 +23,33 @@ DGW.helpers.toggleClass = function(obj, className) {
 DGW.helpers.hasClass = function(obj, className) {
     return new RegExp(className).test(obj.className);
 };
+
+DGW.helpers.setDrawCountdown = function(dt, elem) {
+    var end = new Date(dt);
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+            clearInterval(timer);
+            elem.innerHTML = 'EXPIRED!';
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        elem.innerHTML = days + 'd ';
+        elem.innerHTML += hours + 'h ';
+        elem.innerHTML += minutes + 'm ';
+        elem.innerHTML += seconds + 's';
+    }
+    timer = setInterval(showRemaining, 1000);
+};
--- widget/app/js/init.js
+++ widget/app/js/init.js
@@ -2,13 +2,22 @@ var DGW = {
     templates: {},
     main: {
         methods: {},
+        elements: {
+            pages: {}
+        },
         currentState: ''
     },
     side: {
-        methods: {}
+        methods: {},
+        elements: {}
     },
     global: {
         authorized: false,
+        api: {
+            apiKey: '56A81D28-F711-415A-9F03-CBA9347211C6',
+            requests: {}
+        },
+        elements: {},
         methods: {}
     },
     states: {},
