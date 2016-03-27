629279740d7f5da7aa593dcd3a301f60743f4d63
kartamyshev <kartamyshev.k@yandex.ru>
2016-03-27 11:09:12 +0300

    fixtures proto layout

--- quiz-app/TODO.md
+++ quiz-app/TODO.md
@@ -5,3 +5,4 @@
 - create aliases (review if needed) for most useful cases (css, utils etc)
 - remove jade-loader and all jade files (or use jade?)
 - include roboto as a font (google, locally?)
+- get rid of 'isomorphic-style-loader/lib/withStyles' (strange issues happens, not needed)
--- quiz-app/src/components/Header/Header.js
+++ quiz-app/src/components/Header/Header.js
@@ -18,11 +18,75 @@ class Header extends Component {
   render() {
     return (
       <div className={s.root}>
-        <div className={s.menuIcon}>
-          <img src={require('./menu.svg')} alt="Menu" />
+        
+        <div className={s.headerSection}>
+          <div className={s.menuIcon}>
+            <img src={require('./menu.svg')} alt="Menu" />
+          </div>
+          <div className={s.titleSection}>Fixtures</div>
+          <div className={s.points}>210 pts</div>
         </div>
-        <div className={s.titleSection}>Fixtures</div>
-        <div className={s.points}>210 pts</div>
+
+
+        {/* TODO: Move from here to separate module/component/part */}
+        <div className={s.contentSection}>
+
+          <div className={s.dateSortedSection}>
+            <div className={s.date}>
+              23 April - Premier League
+            </div>
+            
+            <div className={s.event}>
+              <div className={s.logos}></div>
+              <div className={s.info}>
+                <div className={s.participants}>
+                  Everton vs Chelsea
+                </div>
+                <div className={s.details}>
+                  19:00, 3thd tour, london
+                </div>
+              </div>
+              <div className={s.further}></div>
+            </div>
+
+            <div className={s.event}>
+              <div className={s.logos}></div>
+              <div className={s.info}>
+                <div className={s.participants}>
+                  Everton vs Chelsea
+                </div>
+                <div className={s.details}>
+                  19:00, 3thd tour, london
+                </div>
+              </div>
+              <div className={s.further}></div>
+            </div>
+            
+          </div>
+
+          <div className={s.dateSortedSection}>
+            <div className={s.date}>
+              23 April - Premier League
+            </div>
+            <div className={s.event}>
+              <div className={s.logos}></div>
+              <div className={s.info}>
+                <div className={s.participants}>
+                  Everton vs Chelsea
+                </div>
+                <div className={s.details}>
+                  19:00, 3thd tour, london
+                </div>
+              </div>
+              <div className={s.further}></div>
+            </div>
+          </div>
+
+        </div>
+        {/* Move from here to separate module/component/part */}
+
+
+
       </div>
     );
   }
