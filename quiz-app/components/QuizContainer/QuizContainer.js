51e26e8a2617fe55ebfbd66d809c28c581db971e
Pavel Zhytko <pjitco@gmail.com>
2016-04-05 04:25:12 +0300

    add QuizContainer component - container for single quiz/several steps

---
+++ quiz-app/components/QuizContainer/QuizContainer.js
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import ProgressBar from '../ProgressBar';
+import QuizItemType1 from '../QuizItemType1';
+import QuizItemType2 from '../QuizItemType2';
+import QuizItemType3 from '../QuizItemType3';
+
+import '../quiz.scss'
+
+const type2componet = {
+	'xxx': QuizItemType1,
+	'yyy': QuizItemType2,
+	'zzz': QuizItemType3
+};
+
+
+class QuizContainer extends Component {
+
+	static propTypes = {
+		data: PropTypes.array.isRequired
+	};
+
+	render() {
+		const { data } = this.props;
+		const quizes = data.map(({ type, ...rest }) => {
+			const QuizItem = type2componet[type];
+			return (
+				<QuizItem key={ "type" + Math.random() } {...rest} />
+			);
+		});
+
+		return (
+			<div className="quiz-container">
+				<ProgressBar total={ data.length } current={1} />
+				{ quizes }
+			</div>
+		);
+	}
+}
+
+export default QuizContainer;
