26ea220efcddd87287fce9d7b7b3251813da7ec1
Pavel Zhytko <pjitco@gmail.com>
2016-04-29 03:17:04 +0300

    finish Loaderbord tabs with groups (not clickable)

--- quiz-app/components/LeaderBoard/LeaderBoard.js
+++ quiz-app/components/LeaderBoard/LeaderBoard.js
@@ -5,52 +5,6 @@ import Logo from '../UserProfile/Logo.js';
 import './leader-board.scss';
 
 
-const UserListOfTop = ({ top3 }) => {
-	const top3cols = top3.map((user, i) => {
-		const {userId, picture, name, answers, rank} = user;
-
-		return (
-			<Link key={`top-${i}`} className="col" to="./profile" state={ {userId} }>
-				<Logo src={ picture } rank={ rank }/>
-				<div className="user-name">{ name }</div>
-				<div className="user-answers">{ answers } answers</div>
-			</Link>
-		);
-	});
-
-	return (
-		<div className="user-cols-3">
-			{ top3cols }
-		</div>
-	);
-};
-
-
-const UserListOfAll = ({ list, profileRank }) => {
-	const playerItems = list.map((user, i) => {
-		const {userId, picture, name, answers, rank} = user;
-		const myselfClass = (rank === profileRank) ? 'myself' : '';
-
-		return (
-			<Link key={`user-${i}`} className={"user-list-item " + myselfClass} to="./profile" state={ {userId} }>
-				<div className="user-rank">{ rank }</div>
-				<Logo src={picture}/>
-				<div className="user-details">
-					<div className="user-name large">{ name }</div>
-					<div className="user-answers large">{ answers } answers</div>
-				</div>
-			</Link>
-		);
-	});
-
-	return (
-		<ul className="user-list">
-			{ playerItems }
-		</ul>
-	);
-};
-
-
 class LeaderBoard extends Component {
 
 	static propTypes = {
@@ -60,17 +14,47 @@ class LeaderBoard extends Component {
 	};
 
 	render() {
-		const { top3, users, profile: {rank} } = this.props;
-		const rankTitle = `You are ranked ${rank}`;
+		const { top3, users, profile: {rank: myRank} } = this.props;
+		const rankTitle = `You are ranked ${myRank}`;
+		const top3cols = top3.map((user, i) => {
+			const {userId, picture, name, answers, rank} = user;
+
+			return (
+				<Link key={`top-user-${i}`} className="col" to="./profile" state={ {userId} }>
+					<Logo src={ picture } rank={ rank }/>
+					<div className="user-name">{ name }</div>
+					<div className="user-answers">{ answers } answers</div>
+				</Link>
+			);
+		});
+		const playerItems = users.map((user, i) => {
+			const {userId, picture, name, answers, rank} = user;
+			const myselfClass = (rank === myRank) ? 'myself' : '';
+
+			return (
+				<Link key={`user-${i}`} className={"user-list-item " + myselfClass} to="./profile" state={ {userId} }>
+					<div className="user-rank">{ rank }</div>
+					<Logo src={picture}/>
+					<div className="user-details">
+						<div className="user-name large">{ name }</div>
+						<div className="user-answers large">{ answers } answers</div>
+					</div>
+				</Link>
+			);
+		});
 
 		return (
 			<div className="screen-content">
 				<Section title="TOP players">
-					<UserListOfTop top3={ top3 }/>
+					<div className="user-cols-3">
+						{ top3cols }
+					</div>
 				</Section>
 
 				<Section title={ rankTitle }>
-					<UserListOfAll list={ users } profileRank={rank}/>
+					<ul className="user-list">
+						{ playerItems }
+					</ul>
 				</Section>
 			</div>
 		);
@@ -88,15 +72,44 @@ export class LeaderBoardGroup extends Component {
 	render() {
 		const { top3, groups } = this.props;
 		const rankTitle = `You are in 4 groups`;
+		const top3cols = top3.map((group, i) => {
+			const {picture, name, members, rank} = group;
+
+			return (
+				<div key={`top-group-${i}`} className="col">
+					<Logo src={ picture } rank={ rank }/>
+					<div className="user-name">{ name }</div>
+					<div className="user-answers">{ members } members</div>
+				</div>
+			);
+		});
+		const groupItems = groups.map((user, i) => {
+			const {picture, name, members, rank} = user;
+
+			return (
+				<div key={`group-${i}`} className={"user-list-item"}>
+					<div className="user-rank">{ rank }</div>
+					<Logo src={picture}/>
+					<div className="user-details">
+						<div className="user-name large">{ name }</div>
+						<div className="user-answers large">{ members } members</div>
+					</div>
+				</div>
+			);
+		});
 
 		return (
 			<div className="screen-content">
 				<Section title="TOP groups">
-					<UserListOfTop top3={ top3 }/>
+					<div className="user-cols-3">
+						{ top3cols }
+					</div>
 				</Section>
 
 				<Section title={ rankTitle }>
-					<UserListOfAll list={ groups } profileRank="123"/>
+					<ul className="user-list">
+						{ groupItems }
+					</ul>
 				</Section>
 			</div>
 		);
