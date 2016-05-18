84c24fd58295f629055c05cc9c73b4730453c7a1
Pavel Zhytko <pjitco@gmail.com>
2016-05-18 23:09:11 +0300

    fix FAN-2234 - textual changes

--- quiz-app/components/PartnersContainer/data.js
+++ quiz-app/components/PartnersContainer/data.js
@@ -1,5 +1,6 @@
-const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
-	' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+const text = (name) => `Get £20 in free bets when you join ${name} today.
+ Simply register your account and get your £20 in free bets instantly!
+ You can get £20 in Free Bets if you haven't held an account before!`;
 const mailto = 'mailto:?to=&subject=some%20subject&body=some%20body';
 const sms = 'sms:';
 
@@ -8,9 +9,9 @@ const partners = [
 		partnerId: 1,
 		name: 'Coral',
 		picture: require("../../static/images/partner-picture.png"),
-		text: text,
+		text: text('Coral'),
 		signupLink: '//affiliate.coral.co.uk/processing/clickthrgh.asp?btag=a_20912b_2',
-		privacyLink: '//google.com',
+		privacyLink: '//coral-eng.custhelp.com/app/answers/detail/a_id/2132/~/privacy-policy',
 		facebookLink: '//facebook.com',
 		mailtoLink: mailto,
 		smsLink: sms,
@@ -19,9 +20,9 @@ const partners = [
 		partnerId: 2,
 		name: 'Sky bet',
 		picture: require("../../static/images/partner-picture2.png"),
-		text: text,
+		text: text('Sky Bet'),
 		signupLink: 'http://affiliatehub.skybet.com/processing/clickthrgh.asp?btag=a_19010b_2',
-		privacyLink: '//google.com',
+		privacyLink: '//support.skybet.com/app/answers/detail/a_id/143/~/privacy-and-cookies-notice',
 		facebookLink: '//facebook.com',
 		mailtoLink: mailto,
 		smsLink: sms,
