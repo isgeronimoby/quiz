3b6d153f1dbb14a28ceb19a405058b76d5c3c6a2
Pavel Zhytko <pjitco@gmail.com>
2016-05-25 00:21:45 +0300

    fix FAN-2284 - changed bet-exit layout & added static image

--- quiz-app/components/QuizBetExit/QuizBetExit.js
+++ quiz-app/components/QuizBetExit/QuizBetExit.js
@@ -6,22 +6,21 @@ import './QuizBetExit.scss';
 class QuizBetExit extends Component {
 
 	static propTypes = {
-		nextDrawItem: PropTypes.object.isRequired
 	};
 
 	render() {
-		const { drawId, prizeImageUrl } = this.props.nextDrawItem;
-
 		return (
 			<div className="quiz-exit">
-				<Link className="big-image" to="./draw" query={ {drawId} }>
-					<img src={ prizeImageUrl }/>
-				</Link>
+				<div className="big-image-panel">
+					<div className="big-image">
+						<img src={ require('../../static/images/mug.png') }/>
+					</div>
 
-				<Link className="big-btn money-btn" to="./partners">
-					SignUp to our betting partners<br/>
-					<span className="btn-text-sm">and earn a +500 points</span>
-				</Link>
+					<Link className="big-btn money-btn" to="./partners">
+						SignUp to our betting partners<br/>
+						<span className="btn-text-sm">and win great prizes</span>
+					</Link>
+				</div>
 
 				<Link className="big-btn share-btn" to="./fixtures">
 					Return to the other matches
--- quiz-app/pages/bet-exit.js
+++ quiz-app/pages/bet-exit.js
@@ -1,49 +1,18 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { getNextDrawItem } from '../lib/utils.js';
-import { fetchDrawsIfNeeded } from '../flux/actions';
-import { Fetching } from '../components/Layout';
-import QuizBetExit from '../components/QuizBetExit';
+	import QuizBetExit from '../components/QuizBetExit';
 
-class Exit extends Component {
+class BetExit extends Component {
 
 	static title = 'Score Predictor';
 
-	static propTypes = {
-		// from store
-		nextDrawItem: PropTypes.object.isRequired,
-		fetchDraws: PropTypes.func.isRequired,
-	};
-
-	componentDidMount() {
-		this.props.fetchDraws(); // need to get nextDrawItem
-	}
+	static propTypes = {};
 
 	render() {
-		const { nextDrawItem } = this.props;
-
-		if (nextDrawItem.isFetching) {
-			return <Fetching/>;
-		}
-
 		return (
-			<QuizBetExit nextDrawItem={ nextDrawItem }/>
+			<QuizBetExit/>
 		);
 	}
 }
 
-// Connect to store
-//
-const mapStateToProps = (state) => {
-	return {
-		nextDrawItem: getNextDrawItem(state.draws.list) || {isFetching: true},
-	};
-};
-const mapDispatchToProps = (dispatch) => {
-	return {
-		fetchDraws: () => dispatch(fetchDrawsIfNeeded()),
-	};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Exit);
+export default BetExit;
 
