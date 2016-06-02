425684ad716de8a3e5c94ed3bbf5026fd07b7b96
Pavel Zhytko <pjitco@gmail.com>
2016-06-03 01:55:39 +0300

    fix FAN-2308 - re-impl loader with css for perf

--- quiz-app/components/Layout/Layout.js
+++ quiz-app/components/Layout/Layout.js
@@ -10,7 +10,7 @@ import './Layout.scss';
 export const Fetching = () => {
 	return (
 		<div className="fetching">
-			<img src={ require('../../static/images/loader.svg') }/>
+			<div className="loader"></div>
 		</div>
 	);
 };
